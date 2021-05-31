1edf346cb59e975ad45aa965fbcbc4b658bf6783
Pratyush Bhatt <Pratyushbhatt1617@gmail.com>
2021-05-31 08:32:55 +0530

    This commit is till I have implemented the Actvity scheduler, fixed all the possible errors,commit is perfect across all the files(Safe backup point), now only Implementing graph remains

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// CONDITIONAL UI
+
 const signBoard = document.querySelector(".carousel-hide");
 const logo = document.querySelector("#logout-btn");
 const loginNavs = document.querySelectorAll(".login-nav");
@@ -17,4 +19,44 @@ const setupUI = (user) => {
     if (signBoard != null) signBoard.style.display = "block";
     if (logo != null) logo.style.display = "none";
   }
+};
+
+// Setup BIO
+const appendingDIV = document.querySelector(".append-here");
+const setupBIO = (data) => {
+  // console.log(data, "hello");
+  console.log(data, "helloooo");
+  const info = data.data();
+  console.log(info);
+  const appendingBio = `
+      <p class="col-md-8 fs-4">${info.email}</p>
+      <p class="col-md-8 fs-4">${info.dateOfBirth}</p>
+    `;
+  if (appendingDIV) appendingDIV.innerHTML = appendingBio;
+};
+
+// SETUP ACTIVITIES
+const appendActivityDiv = document.querySelector(".append-activity-div");
+const setupActivities = (data) => {
+  // console.log(data);
+  let finalAppend = "";
+  data.forEach((doc) => {
+    const acti = doc.data();
+    console.log(acti);
+    const str = `<div class="col-lg-4 col-md-6 mb-2">
+              <div class="card .h-100">
+                <div class="card-header text-white bg-secondary">
+                  <h3>${acti.Sport}</h3>
+                  <small>${acti.Location}</small>
+                </div>
+                <div class="card-body">
+                  <p>Date: ${acti.date}</p>
+                  <p>Start Time: ${acti.StartTime}</p>
+                  <p>End Time: ${acti.EndTime}</p>
+                </div>
+              </div>
+            </div>`;
+    finalAppend += str;
+  });
+  if (appendActivityDiv) appendActivityDiv.innerHTML = finalAppend;
 };
